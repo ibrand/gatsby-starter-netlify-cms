26f033b63d73b430ed656a8d84f507485f1a7f4b
Ilona Brand <theilonabrand@gmail.com>
2019-07-02 17:25:59 -0400

    add hashtags to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,20 +17,20 @@ const Footer = class extends React.Component {
               className="logo"
             />
           </a>
-          <a title="facebook" href="https://facebook.com">
+          <a title="facebook" href="https://www.facebook.com/hashtag/institutionalBullying" target="_blank" rel="noopener noreferrer">
             <img
               src={facebook}
               alt="Facebook"
             />
           </a>
-          <a title="twitter" href="https://twitter.com">
+          <a title="twitter" href="https://twitter.com/search?q=%23institutionalBullying" target="_blank" rel="noopener noreferrer">
             <img
               className="fas fa-lg"
               src={twitter}
               alt="Twitter"
             />
           </a>
-          <a title="instagram" href="https://instagram.com">
+          <a title="instagram" href="https://www.instagram.com/explore/tags/institutionalBullying/" target="_blank" rel="noopener noreferrer">
             <img
               src={instagram}
               alt="Instagram"
