979e01e01c69ea1da923a31e60918ea81e44bfba
Ilona Brand <theilonabrand@gmail.com>
2019-09-10 17:27:13 -0500

    try to adjust the gatsby-config to account for the new gallery changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/img`,
+        path: `${__dirname}/static/img`,
         name: 'images',
       },
     },
