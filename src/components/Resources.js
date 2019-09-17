621d2556e216f8ea63344be204fbc416a45fd8c7
Ilona Brand <theilonabrand@gmail.com>
2019-09-17 14:16:23 -0500

    Resources: add endorsers category to the CMS and the resources page and make sure a category doesn't appear if it has no contents

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -4,44 +4,60 @@ import { graphql, StaticQuery } from 'gatsby'
 
 class Resources extends React.Component {
   render() {
-    const { toolkit, videos } = this.props.data
-    const { edges: toolkit_resources } = toolkit
-    const { edges: video_resources } = videos
+    const { toolkit, videos, endorsers } = this.props.data
+    const toolkit_resources = toolkit.edges.length > 0 ? toolkit.edges : '';
+    const video_resources = videos.edges.length > 0 ? videos.edges : '';
+    const endorser_resources = endorsers.edges.length > 0 ? endorsers.edges : '';
 
     return (
       <React.Fragment>
+        {toolkit_resources &&
         <section className="resource-container">
           <h2>Toolkit:</h2>
           <ul>
-          {toolkit_resources &&
-          toolkit_resources.map(({ node: post }, index) => (
-            <li  className="resource" key={post.id}>
-              <a href={post.frontmatter.resource.publicURL} target="_blank" rel="noopener noreferrer"  key={post.id}>
-                {post.frontmatter.title}
-              </a>
-            </li>
-          ))
-          }
+            {toolkit_resources.map(({node: post}, index) => (
+              <li className="resource" key={post.id}>
+                <a href={post.frontmatter.resource.publicURL} target="_blank" rel="noopener noreferrer" key={post.id}>
+                  {post.frontmatter.title}
+                </a>
+              </li>
+            ))
+            }
           </ul>
-        </section>
+        </section>}
+        {video_resources &&
         <section className="resource-container">
           <h2>Videos:</h2>
           <ul>
-          {video_resources &&
-          video_resources.map(({ node: post }, index) => (
+            {video_resources.map(({ node: post }, index) => (
             <li  className="resource" key={post.id}>
               <div className="video-text">
-              <strong key={post.id}>
-                {post.frontmatter.title}
-              </strong>
-              {post.frontmatter.description && <p>{post.frontmatter.description}</p>}
+                <strong key={post.id}>
+                  {post.frontmatter.title}
+                </strong>
               </div>
               <iframe src={post.frontmatter.url}/>
             </li>
           ))
           }
           </ul>
-        </section>
+        </section>}
+        {endorser_resources &&
+        <section className="resource-container">
+          <h2>Endorsers:</h2>
+          <ul>
+            {endorser_resources.map(({ node: post }, index) => (
+              <li  className="resource" key={post.id}>
+                <div className="video-text">
+                  <strong key={post.id}>
+                    <a href={post.frontmatter.url}>{post.frontmatter.title}</a>
+                  </strong>
+                </div>
+              </li>
+            ))
+            }
+          </ul>
+        </section>}
       </React.Fragment>
     )
   }
@@ -91,6 +107,22 @@ export default () => (
         videos: allMarkdownRemark(
           sort: { order: DESC, fields: [frontmatter___date] }
           filter: { frontmatter: { templateKey: { eq: "resource" }, category: {eq: "videos"} } }
+        ) {
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+                url
+                date(formatString: "MMMM DD, YYYY")
+                category
+              }
+            }
+          }
+        },
+        endorsers: allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          filter: { frontmatter: { templateKey: { eq: "resource" }, category: {eq: "endorsers"} } }
         ) {
           edges {
             node {
