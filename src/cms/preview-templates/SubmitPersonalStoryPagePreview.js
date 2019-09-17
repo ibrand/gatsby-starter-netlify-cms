74c01397a16886df0763f5aa6b9ff091f767d83b
Ilona Brand <theilonabrand@gmail.com>
2019-09-17 14:48:43 -0500

    Submit Personal Story: add ability to edit subtitle in the CMS

---
+++ src/cms/preview-templates/SubmitPersonalStoryPagePreview.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { AboutPageTemplate } from '../../templates/about-page'
+
+const AboutPagePreview = ({ entry }) => {
+  const data = entry.getIn(['data']).toJS()
+  if (data) {
+    return (
+      <AboutPageTemplate
+        html={data.body}
+        bannerImgSizes={{'':''}}
+      />
+    )
+  } else {
+    return (<div>Loading...</div>)
+  }
+}
+
+AboutPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+}
+
+export default AboutPagePreview
