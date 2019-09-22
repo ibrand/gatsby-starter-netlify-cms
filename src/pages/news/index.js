ce007e3223c7cebbb9925c52ad612f7027f65fd2
Ilona Brand <theilonabrand@gmail.com>
2019-09-22 17:02:52 -0500

    News Page: Allow for scrolling outside of the container on desktop and mobile

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -3,16 +3,62 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import NewsArticles from '../../components/NewsArticles'
 
-export const NewsPageTemplate = () => (
-  <section className="news">
-    <div className="container">
-      <h2 className="page-title">Media Coverage</h2>
-      <div className="scroll-container">
-        <NewsArticles />
-      </div>
-    </div>
-  </section>
-)
+const NewsPageTemplate = class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.scrollContainerRef = React.createRef()
+    this.state = {
+      variableParentOverflow: 'scroll',
+      variableChildOverflow: 'hidden',
+      touchScrollStartingValue: 0
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('touchstart', this.handleTouchScrollStart)
+    window.addEventListener('touchmove', this.handleTouchScrollMove)
+    window.addEventListener('mousewheel', this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('touchstart', this.handleTouchScrollStart)
+    window.removeEventListener('touchmove', this.handleTouchScrollMove)
+    window.removeEventListener('mousewheel', this.handleScroll)
+  }
+
+  handleScroll = (e) => {
+    this.scrollContainerRef.current.scrollTop = this.scrollContainerRef.current.scrollTop + e.deltaY
+  }
+
+  handleTouchScrollStart = (e) => {
+    this.state.touchScrollStartingValue = e.touches[0].pageY
+  }
+
+  handleTouchScrollMove = (e) => {
+    let offset = this.state.touchScrollStartingValue - e.touches[0].pageY
+    this.scrollContainerRef.current.scrollTop = this.scrollContainerRef.current.scrollTop + offset
+  }
+
+  render() {
+    return (
+      <section
+        className="news"
+        style={{overflow: this.state.variableParentOverflow}}
+      >
+        <div className="container">
+          <h2 className="page-title">Media Coverage</h2>
+          <div
+            className="scroll-container"
+            style={{overflow: this.state.variableChildOverflow}}
+            ref={this.scrollContainerRef}
+          >
+            <NewsArticles />
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
 
 const NewsPage = () => {
   return (
