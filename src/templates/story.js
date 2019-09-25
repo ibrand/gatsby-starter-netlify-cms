c76bc114320a44d094d6c781f4f903cf02ebaf55
Ilona Brand <theilonabrand@gmail.com>
2019-09-25 15:06:15 -0500

    Story: fix the repetition of date in the graphql call

--- src/templates/story.js
+++ src/templates/story.js
@@ -84,7 +84,6 @@ export const pageQuery = graphql`
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         who
-        date
         location
         image {
           childImageSharp {
