58f001035013f5611275a9313e41fae25183e2b9
Ilona Brand <theilonabrand@gmail.com>
2019-09-29 10:25:44 -0400

    Header: make the font larger and menu items visible with a hamburger only viable on mobile

---
+++ src/components/NavigationItems.js
@@ -0,0 +1,52 @@
+import {Link} from "gatsby";
+import React from "react";
+
+const NavigationItems = () => (
+  <>
+  <ul>
+    <li className="list-header">
+      <Link to="/">
+        End Institutionalized Bullying in Schools
+      </Link>
+    </li>
+    <li>
+      <Link to="/about">
+        About
+      </Link>
+    </li>
+    <li>
+      <Link to="/read-stories">
+        Read Stories
+      </Link>
+    </li>
+    <li>
+      <Link to="/submit-personal-story">
+        Submit Personal Story
+      </Link>
+    </li>
+    <li>
+      <Link to="/gallery">
+        Gallery
+      </Link>
+    </li>
+    <li>
+      <Link to="/news">
+        Media Coverage
+      </Link>
+    </li>
+    <li>
+      <Link to="/resources">
+        Resources
+      </Link>
+    </li>
+    <li>
+      <Link to="/contact">
+        Contact
+      </Link>
+    </li>
+    <a href="https://drum.ourpowerbase.net/civicrm/contribute/transact?reset=1&id=13" target="_blank" rel="noopener noreferrer" className="donate-button">Donate</a>
+  </ul>
+  </>
+)
+
+export default NavigationItems
